1074c3711ff7d47def5dc1ecc3d61b94cebb6188
Physiccc09 <khylian.griffon-nicolas@supinfo.com>
2025-11-25 19:20:11 +0100

    Worked on html

---
+++ jsx/aftereffects.jsx
@@ -0,0 +1,12 @@
+function CloseAfterEfffects() {
+    app.quit();
+}
+
+function ClearDiskCache() {
+    app.executeCommand(10200);
+}
+
+function RestartExtension(){
+    window.location.reload()
+}
+
