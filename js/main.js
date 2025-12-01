1c817ea1604e8052f5a6e24c5dfa6f632c5373fb
Physic <khylian.griffon-nicolas@supinfo.com>
2025-12-01 16:40:52 +0100

    Add OS detection and path logging in main.js

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//Adobe code (go to jsx and detect OS)
+(function () {
+ 'use strict';
+ var extPath;
+ extPath = location.href;
+	if(getOS() == "MAC") {
+		extPath = extPath.substring(7, extPath.length - 11);
+	}
+	if(getOS() == "WIN") {
+		extPath = extPath.substring(8, extPath.length - 11);
+	}
+
+	console.log("Start");
+	console.log(extPath);
+
+ }());
+
+function goIntoJSX() {
+	
+	}
+
+function getOS() {
+ 		var userAgent = window.navigator.userAgent,
+ 		platform = window.navigator.platform,
+ 		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+ 		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+ 		os = null;
+
+ 		if(macosPlatforms.indexOf(platform) != -1) {
+ 			os = "MAC";
+ 		} else if(windowsPlatforms.indexOf(platform) != -1) {
+ 			os = "WIN";
+ 		}
+ 		return os;
+ 	}
