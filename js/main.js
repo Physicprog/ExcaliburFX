b15e965e6f8eb3cddea0d1bbad2c05b4571ceec9
Physiccc09 <khylian.griffon-nicolas@supinfo.com>
2025-12-18 21:18:28 +0100

    added loader

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 (function () {
 	'use strict';
-	var extPath = location.href;
-
-	if (getOS() === "MAC") {
-		extPath = extPath.substring(7, extPath.length - 11);
-	}
-	if (getOS() === "WIN") {
-		extPath = extPath.substring(8, extPath.length - 11);
-	}
-
 	window.cs = new CSInterface();
-	cs.evalScript('$.evalFile("' + extPath + './../jsx/aftereffects.jsx")');
+	var extPath = cs.getSystemPath(SystemPath.EXTENSION);
+
+	cs.evalScript('$.evalFile("' + extPath + '/jsx/aftereffects.jsx")', function (result) {
+		const loadingOverlay = document.getElementById("loadingOverlay");
+		const content = document.querySelector(".content");
+
+		// Fade out the loading overlay
+		loadingOverlay.style.opacity = "0";
+		loadingOverlay.style.transition = "opacity 0.5s ease";
+
+		// Show the content after the overlay fades out
+		setTimeout(() => {
+			loadingOverlay.style.display = "none";
+			content.style.display = "block";
+		}, 500); // Match the transition duration
+	});
 
 	console.log("Start");
 	console.log(extPath);
@@ -21,19 +27,12 @@ function E(script) {
 	cs.evalScript(script);
 }
 
-function getOS() {
-	var platform = window.navigator.platform;
-	var macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
-	var windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
-	if (macosPlatforms.indexOf(platform) !== -1) return "MAC";
-	if (windowsPlatforms.indexOf(platform) !== -1) return "WIN";
-	return null;
-}
-
-// recharge l'extension After Effects (marche pas ??)
+// recharge l'extension After Effects
 function reloadPanel() {
 	cs.reloadExtension();
 }
+
+
 
 document.addEventListener("keydown", function (e) {
 	if (e.ctrlKey && e.key.toLowerCase() === "r") {
